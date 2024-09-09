cc98a3b4f83d6618aa581b76159a20dc689a5529
GasanaJr <gasanadj2003@gmail.com>
2024-09-09 16:04:41 +0200

    feat(losing): add the update method to reload the scene when health drops

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
-	public float speed;
+	public float speed = 100f;
 	public Rigidbody rb;
 	private int score;
 	public int health;
 
-	// ***** Method 3  | Winner ****************************
+    // ***** Method 3  | Winner ****************************
 
-	void Start()
+    void Start()
 	{
 		score = 0;
 		health = 5;
@@ -46,50 +46,68 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	// ***** Method 1 ****************************
-	// void FixedUpdate()
-	// {
-	// 	var pos = transform.position;
-	// 	if (Input.GetKey(KeyCode.LeftArrow))
-	// 	{
-	// 		pos.x = pos.x - speed * Time.deltaTime;
-	// 	}
-	// 	if (Input.GetKey(KeyCode.RightArrow))
-	// 	{
-	// 		pos.x = pos.x + speed * Time.deltaTime;
-	// 	}
-	// 	if (Input.GetKey(KeyCode.UpArrow))
-	// 	{
-	// 		pos.z = pos.z + speed * Time.deltaTime;
-	// 	}
-	// 	if (Input.GetKey(KeyCode.DownArrow))
-	// 	{
-	// 		pos.z = pos.z - speed * Time.deltaTime;
-	// 	}
-	// 	transform.position = pos;
+    void Update()
+    {
+        // Check if health is zero or less
+        if (health <= 0)
+        {
+            Debug.Log("Game Over!");
 
-	// }
+            // Call the ReloadScene method to handle scene reloading and resetting
+            ReloadScene();
+        }
+    }
 
-	// ***** Method 2 ****************************
-	// void FixedUpdate()
-	// {
-	// 	if (Input.GetKey(KeyCode.UpArrow))
-	// 	{
-	// 		rb.AddForce(0, 0, speed * Time.deltaTime);
-	// 	}
-	// 	if (Input.GetKey(KeyCode.DownArrow))
-	// 	{
-	// 		rb.AddForce(0, 0, -speed * Time.deltaTime);
-	// 	}
-	// 	if (Input.GetKey(KeyCode.LeftArrow))
-	// 	{
-	// 		rb.AddForce(-speed * Time.deltaTime, 0, 0);
-	// 	}
-	// 	if (Input.GetKey(KeyCode.RightArrow))
-	// 	{
-	// 		rb.AddForce(speed * Time.deltaTime, 0, 0);
-	// 	}
-	// }
+    private void ReloadScene()
+    {
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // ***** Method 1 ****************************
+    // void FixedUpdate()
+    // {
+    // 	var pos = transform.position;
+    // 	if (Input.GetKey(KeyCode.LeftArrow))
+    // 	{
+    // 		pos.x = pos.x - speed * Time.deltaTime;
+    // 	}
+    // 	if (Input.GetKey(KeyCode.RightArrow))
+    // 	{
+    // 		pos.x = pos.x + speed * Time.deltaTime;
+    // 	}
+    // 	if (Input.GetKey(KeyCode.UpArrow))
+    // 	{
+    // 		pos.z = pos.z + speed * Time.deltaTime;
+    // 	}
+    // 	if (Input.GetKey(KeyCode.DownArrow))
+    // 	{
+    // 		pos.z = pos.z - speed * Time.deltaTime;
+    // 	}
+    // 	transform.position = pos;
+
+    // }
+
+    // ***** Method 2 ****************************
+    // void FixedUpdate()
+    // {
+    // 	if (Input.GetKey(KeyCode.UpArrow))
+    // 	{
+    // 		rb.AddForce(0, 0, speed * Time.deltaTime);
+    // 	}
+    // 	if (Input.GetKey(KeyCode.DownArrow))
+    // 	{
+    // 		rb.AddForce(0, 0, -speed * Time.deltaTime);
+    // 	}
+    // 	if (Input.GetKey(KeyCode.LeftArrow))
+    // 	{
+    // 		rb.AddForce(-speed * Time.deltaTime, 0, 0);
+    // 	}
+    // 	if (Input.GetKey(KeyCode.RightArrow))
+    // 	{
+    // 		rb.AddForce(speed * Time.deltaTime, 0, 0);
+    // 	}
+    // }
 
 
 }
