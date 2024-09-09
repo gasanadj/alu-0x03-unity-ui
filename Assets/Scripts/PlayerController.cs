ceaf11f584c505e273e20c0204e1a7dd1cc62de8
GasanaJr <gasanadj2003@gmail.com>
2024-09-09 11:25:36 +0200

    feat(playercontroller): add a script for player movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody rb;
+
+	// ***** Method 1 ****************************
+	// void FixedUpdate()
+	// {
+	// 	var pos = transform.position;
+	// 	if (Input.GetKey(KeyCode.LeftArrow))
+	// 	{
+	// 		pos.x = pos.x - speed * Time.deltaTime;
+	// 	}
+	// 	if (Input.GetKey(KeyCode.RightArrow))
+	// 	{
+	// 		pos.x = pos.x + speed * Time.deltaTime;
+	// 	}
+	// 	if (Input.GetKey(KeyCode.UpArrow))
+	// 	{
+	// 		pos.z = pos.z + speed * Time.deltaTime;
+	// 	}
+	// 	if (Input.GetKey(KeyCode.DownArrow))
+	// 	{
+	// 		pos.z = pos.z - speed * Time.deltaTime;
+	// 	}
+	// 	transform.position = pos;
+
+	// }
+
+	// ***** Method 2 ****************************
+	// void FixedUpdate()
+	// {
+	// 	if (Input.GetKey(KeyCode.UpArrow))
+	// 	{
+	// 		rb.AddForce(0, 0, speed * Time.deltaTime);
+	// 	}
+	// 	if (Input.GetKey(KeyCode.DownArrow))
+	// 	{
+	// 		rb.AddForce(0, 0, -speed * Time.deltaTime);
+	// 	}
+	// 	if (Input.GetKey(KeyCode.LeftArrow))
+	// 	{
+	// 		rb.AddForce(-speed * Time.deltaTime, 0, 0);
+	// 	}
+	// 	if (Input.GetKey(KeyCode.RightArrow))
+	// 	{
+	// 		rb.AddForce(speed * Time.deltaTime, 0, 0);
+	// 	}
+	// }
+
+
+	// ***** Method 3  | Winner ****************************
+	void FixedUpdate()
+	{
+		var xAxis = Input.GetAxis("Horizontal");
+		var zAxis = Input.GetAxis("Vertical");
+
+		var movementVector = new Vector3(xAxis, 0, zAxis);
+		rb.AddForce(movementVector * speed);
+	}
+}
